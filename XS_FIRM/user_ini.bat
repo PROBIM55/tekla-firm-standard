ab58bbc8be39c4188a864f22b3210edb1e743129
gpsm-nm <connector@local>
2026-04-16 14:12:24 +0300

    firm 2026.03.25: Публикация из Structura Connector

--- XS_FIRM/user_ini.bat
+++ XS_FIRM/user_ini.bat
@@ -1,12 +1,18 @@
 @echo off
 :: укажите версию и путь к серверу
 set VERSION=2025.0
-set SERVER_FILE=c:\Company\user.ini
+set SERVER_FILE=c:\Company\TeklaFirm\user.ini
 set LOCAL_DIR=%LOCALAPPDATA%\Trimble\Tekla Structures\%VERSION%\UserSettings
 
-:: создаем папку, если ее нет, и копируем файл
+:: создаем папку, если ее нет
 if not exist "%LOCAL_DIR%" mkdir "%LOCAL_DIR%"
-copy /y "%SERVER_FILE%" "%LOCAL_DIR%\user.ini"
 
-echo Gotovo!
+:: проверяем наличие исходного файла и копируем
+if exist "%SERVER_FILE%" (
+    copy /y "%SERVER_FILE%" "%LOCAL_DIR%\user.ini"
+    echo Gotovo!
+) else (
+    echo Error: Server file not found!
+)
+
 pause
